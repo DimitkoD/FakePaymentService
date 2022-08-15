e3a2b5a8e74ba159d5ac744a45b295b3b1d629a6
DimitkoD <84143276+DimitkoD@users.noreply.github.com>
2022-08-15 16:17:31 +0300

    initialCommit

---
+++ src/main/java/com/example/fakepaymentservice/FakePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.fakepaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakePaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakePaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fakepaymentservice/FakePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fakepaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakePaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
